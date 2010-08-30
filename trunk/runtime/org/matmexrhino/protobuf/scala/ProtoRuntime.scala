b4f32599af7b7ec8805934495bac2047bdd0199c
eugene.vigdorchik <eugene.vigdorchik@ae20084a-0359-de83-1199-3a4ff898213f>
2010-08-30 07:31:32 +0000

    it's logical to have abstract methods rather than fields in an abstract class

--- trunk/runtime/org/matmexrhino/protobuf/scala/ProtoRuntime.scala
+++ trunk/runtime/org/matmexrhino/protobuf/scala/ProtoRuntime.scala
@@ -8,11 +8,14 @@ class SeqOpt[B, T] (f : (B, T) => Unit) {
   def apply(opts : T*) : B => Unit = { b => opts.foreach { f(b, _) } }
 }
 
-abstract class MessageObject[B](newInstance : => B, clone : B => B) {
+abstract class MessageObject[B] {
   def apply(instance : B)(opts : (B => Unit)*) : B = {
     impl(clone(instance))(opts : _*)
   }
   def apply(opts : (B => Unit)*) : B = impl(newInstance)(opts : _*)
+
+  def newInstance : B
+  def clone(b : B) : B
 
   private def impl(instance : B)(opts : (B => Unit)*) : B = {
     for (opt <- opts) opt(instance)
