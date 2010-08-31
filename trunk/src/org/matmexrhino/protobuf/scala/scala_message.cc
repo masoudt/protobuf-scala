d55c2c42b869e1089e48c1fd6795abd7b35958f9
eugene.vigdorchik <eugene.vigdorchik@ae20084a-0359-de83-1199-3a4ff898213f>
2010-08-31 09:21:32 +0000

    extensions (nested in a message) supported

--- trunk/src/org/matmexrhino/protobuf/scala/scala_message.cc
+++ trunk/src/org/matmexrhino/protobuf/scala/scala_message.cc
@@ -24,13 +24,13 @@ using namespace google::protobuf::compiler::java;
 using google::protobuf::FieldDescriptor;
 
 namespace {
-  string GetScalaType(const FieldDescriptor* descriptor) {
+  string GetScalaType(const FieldDescriptor* descriptor, bool box) {
     switch (GetJavaType(descriptor)) {
-      case JAVATYPE_INT: return "scala.Int";
-      case JAVATYPE_LONG: return "scala.Long";
-      case JAVATYPE_FLOAT: return "scala.Float";
-      case JAVATYPE_DOUBLE: return "scala.Double";
-      case JAVATYPE_BOOLEAN: return "scala.Boolean";
+      case JAVATYPE_INT: return box ? "java.lang.Integer" : "scala.Int";
+      case JAVATYPE_LONG: return box ? "java.lang.Long" : "scala.Long";
+      case JAVATYPE_FLOAT: return box ? "java.lang.Float" : "scala.Float";
+      case JAVATYPE_DOUBLE: return box ? "java.lang.Double" : "scala.Double";
+      case JAVATYPE_BOOLEAN: return box ? "java.lang.Boolean" : "scala.Boolean";
       case JAVATYPE_STRING: return "java.lang.String";
       case JAVATYPE_BYTES: return "com.google.protobuf.ByteString";
       case JAVATYPE_ENUM: return ClassName(descriptor->enum_type());
@@ -55,6 +55,31 @@ namespace scala {
 ScalaMessageGenerator::ScalaMessageGenerator() {}
 ScalaMessageGenerator::~ScalaMessageGenerator() {}
 
+void ScalaMessageGenerator::PrintField(const FieldDescriptor* field, 
+				       const string& classname,
+				       const string& constr_value_part,
+				       bool box_field_type,
+				       Printer* printer) {
+  map<string, string> vars;
+  vars["name"] =  field->name();
+  vars["classname"] = classname + ".Builder";
+  vars["opttype"] = GetScalaType(field, box_field_type);
+  if (field->is_repeated()) {
+    vars["sort"] = "Seq";
+    vars["constr"] = "(_ add" + constr_value_part + ")";
+  } else  if (field->type() == FieldDescriptor::TYPE_MESSAGE) {
+    vars["sort"] = "Message";
+    vars["constr"] = "(_ set" + constr_value_part + ")"
+      "(" + ObjectName(field->message_type()) + ")";
+  } else {
+    vars["sort"] = "";
+    vars["constr"] = "(_ set" + constr_value_part + ")";
+  }
+  printer->Print(vars, 
+		 "val $name$ = new org.matmexrhino.protobuf.scala.$sort$Opt"
+		 "[$classname$, $opttype$]$constr$\n");
+}
+
 void ScalaMessageGenerator::Generate(const Descriptor* descriptor,
 				     Printer* printer) {
   string classname = ClassName(descriptor);
@@ -70,27 +95,15 @@ void ScalaMessageGenerator::Generate(const Descriptor* descriptor,
   printer->Print(objvars, "def clone(b : $classname$.Builder) = b.clone\n");
   for(int i = 0; i < descriptor->field_count(); ++i) {
     const FieldDescriptor* field = descriptor->field(i);
-    map<string, string> vars;
-    vars["name"] =  field->name();
-    vars["classname"] = classname + ".Builder";
-    vars["opttype"] = GetScalaType(field);
-    if (field->is_repeated()) {
-      vars["sort"] = "Seq";
-      vars["constr"] =
-	"(_ add" + UnderscoresToCapitalizedCamelCase(field) + " _)";
-    } else  if (field->type() == FieldDescriptor::TYPE_MESSAGE) {
-      vars["sort"] = "Message";
-      vars["constr"] =
-	"(_ set" + UnderscoresToCapitalizedCamelCase(field) + " _)"
-	"(" + ObjectName(field->message_type()) + ")";
-    } else {
-      vars["sort"] = "";
-      vars["constr"] =
-	"(_ set" + UnderscoresToCapitalizedCamelCase(field) + " _)";
-    }
-    printer->Print(vars, 
-		   "val $name$ = new org.matmexrhino.protobuf.scala.$sort$Opt"
-		   "[$classname$, $opttype$]$constr$\n");
+    PrintField(field, classname, 
+	       UnderscoresToCapitalizedCamelCase(field) + " _", false, printer);
+  }
+
+  for(int i = 0; i < descriptor->extension_count(); ++i) {
+    const FieldDescriptor* field = descriptor->extension(i);
+    const string generated_extension = classname + "." + field->name();
+    PrintField(field, ClassName(field->containing_type()),
+	       "Extension(" + generated_extension + ", _)", true, printer);
   }
 
   printer->Print("\n");
